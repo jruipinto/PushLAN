2471d21621b45fa4d862a7405e0a52d3988347cb
jruipinto <44852805+jruipinto@users.noreply.github.com>
2020-11-15 15:41:31 +0000

    perf(web-client): replace download function
    replace download function by html a[download]
    improving performance and discarding
    usage of streamsaver.js and it's
    "main-in-the-middle"(mitm) worker

--- packages/web-client/src/app/components/list-item/list-item.component.ts
+++ packages/web-client/src/app/components/list-item/list-item.component.ts
@@ -1,34 +1,46 @@
-import { Component, OnInit, ChangeDetectionStrategy, Input } from '@angular/core';
+import {
+  Component,
+  OnInit,
+  ChangeDetectionStrategy,
+  Input,
+} from '@angular/core';
 import { FilesService } from 'src/app/shared/services/files.service';
 import { ListItem } from './list-item.model';
 import { environment } from 'src/environments/environment';
-import { downloadFile } from './functions';
+// import { downloadFile } from './functions';
 
 @Component({
   selector: 'app-list-item',
   templateUrl: './list-item.component.html',
   styleUrls: ['./list-item.component.scss'],
-  changeDetection: ChangeDetectionStrategy.OnPush
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class ListItemComponent implements OnInit {
   @Input() file: ListItem;
-  constructor(private filesService: FilesService) { }
+  environment = environment;
+  constructor(private filesService: FilesService) {}
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
   openFolder(item: ListItem): void {
     if (item.isDirectory) {
-      this.filesService.find$({
-        query: {
-          path: item.path.replace(/\\/g, '/')
-        }
-      }).subscribe();
+      this.filesService
+        .find$({
+          query: {
+            path: item.path.replace(/\\/g, '/'),
+          },
+        })
+        .subscribe();
     }
   }
-  download(item: ListItem): void {
-    if (item.isFile) {
-      downloadFile(environment.apiURL + 'download?path=' + item.path, item.name)
-        .then(() => { });
-    }
-  }
+  //
+  // the following download function isn't used anymore but I keep it here
+  // for the future event of need to download file with the help
+  // of streamsaver.js
+  //
+  // download(item: ListItem): void {
+  //   if (item.isFile) {
+  //     downloadFile(environment.apiURL + 'download?path=' + item.path, item.name)
+  //       .then(() => { });
+  //   }
+  // }
 }
